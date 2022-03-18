b84e04cfdda7894f2efd43434c71fc3abd764e84
Milind <milindanand74@gmail.com>
2022-03-18 22:21:00 +0530

    datas done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import HeadingContent from "./components/Heading/HeadingContent";
+import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeadingContent />
+      
     </div>
   );
 }
