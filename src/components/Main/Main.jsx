5a7941e84eb4b83ebd3a76402196b6ca1ebaf40c
Milind <milindanand74@gmail.com>
2022-03-18 23:03:32 +0530

    left component done

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Main.css"
+import Leftside from "../Leftside/Leftside";
+
+function Main() {
+  return (
+    <div className="mainContainer">
+      <Leftside />
+    </div>
+  );
+}
+
+export default Main;
