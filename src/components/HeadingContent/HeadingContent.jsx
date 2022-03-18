6ca88ed3432f7dcd7ea79b39d823cea5097d984a
Milind <milindanand74@gmail.com>
2022-03-18 21:52:35 +0530

    navbar done

---
+++ src/components/HeadingContent/HeadingContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HeadingContent.css";
+
+function HeadingContent() {
+  return (
+    <div className="heading-content">
+      <h1>All deals and coupons.</h1>
+      <p>
+        The best online deals and coupons, including Klarna exclusives, updated
+        daily.
+      </p>
+      <p></p>
+    </div>
+  );
+}
+
+export default HeadingContent;
